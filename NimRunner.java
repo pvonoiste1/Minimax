1964fda9cb3ea00eed4dc135121d070f8ab67e67
Patricia von OIste <patricia@vonoiste.com>
2023-04-25 17:55:41 -0400

    worked on read me

--- NimRunner.java
+++ NimRunner.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class NimRunner{
     public static void main(String[] args){
 
@@ -5,32 +6,87 @@ public class NimRunner{
 
     public static boolean runGame(){
         //hard code the number of pieces
-        int numPieces = 6;
+        int pieces = 6;
+        //start xWins with true
+        boolean xWins = true;
         //returning the value of who won
-        while(numPieces>0){
-            int xMove = getXMove(numPieces);
-            numPieces = numPieces-xMove;
-            int yMove = getYMove(numPieces);
-            numPieces = numPieces-yMove;
+        while(pieces>0){ //update the pieces based on X and Y moves
+            if (pieces == 0){ 
+            pieces -= getXMove(pieces);
+            xWins = false; //make xWins false to change the state after each turn
+            }
+            pieces -= getYMove(pieces); //get the move that Y will make
+            if (pieces == 0){
+                break; 
+            }
         }
+        return xWins; 
     }
 
     public static int getXMove(int numPieces){
         //return the number of pieces that you take (from 0-3)
-        int chosenInt = Math.floor(Math.random()*(3-1+1));
-        return chosenInt;
+        return bestMove(numPieces, true); //call bestMove
 
     }
 
     public static int getYMove(int numPieces){
         //return the number of pieces that you take (from 0-3)
-        int chosenInt = Math.floor(Math.random()*(3-1+1));
-        return chosenInt;
+        return bestMove(numPieces, false); //call bestMove
 
     }
 
-    public void display(){
-        
+    public static int minimax(int state, boolean myTurn){
+        if (state == 0 && myTurn){ //base case 1
+            return 1;
+        }
+        else if(state == 0 && !myTurn){ //base case 2
+            return -1;
+        }
+        else{
+            ArrayList<Integer> scores = new ArrayList<> (); //create an AL to hold all of the possible scores
+            for(int i = 1; i<4; i++){
+                if(i<=state){
+                    scores.add(minimax(state-i, !myTurn));
+                }
+            }
+
+            //set default values to max and min
+            int min = scores.get(0); 
+            int max = scores.get(0);
+            //iterate through the size of sizes
+            for(int j = 0; j<scores.size(); j++){
+                if(myTurn && scores.get(j)>max){
+                    max = scores.get(j);
+                }
+                else if(!myTurn && scores.get(j) < min ){
+                    min = scores.get(j);
+                }
+            }
+            if(myTurn){ // if it your turn, maximize
+                return max;
+            }
+            else{ // if it is not your turn, minimize
+                return min;
+            }
+        }    
+    }
+
+    public static int bestMove(int state, boolean myTurn){
+        //returns how many pieces is optimal to take
+        int score = 0; //hold the score given by each move
+        for(int i=1; i<4; i++){
+            if(i<= state){
+                score = minimax(state-1, myTurn); //recursive element, call minimax to test that move and to save the cosre
+                if(myTurn && score == 1){
+                    return i;
+                }
+                else if(!myTurn && score == 1){
+                    return i;
+                }
+            }
+        }
+        //if you never get 1 or -1, return a random move
+        return 3;
     }
 
 }
---
+++ README.md
@@ -0,0 +1,14 @@
+# Minimax by Patricia von Oiste
+
+## Background and Motivation
+Nim is a game in which two players take turns removing objects from a certain number of piles. Each player removes at least one object, but they must come from the same pile. The objective of the game is to avoid taking the last object. The player who takes the last object looses the game and the other player wins. 
+
+## Minimax algorithm
+The minimax algorithm is a decision based strategy used to minimize the possible loss or worst case scenario and to maximize the chances of winning. The minmimax algorithm can be applied to zero-sum game theory, which covers cases where players alternate moves. Minimax is used to optimize your chances of winning given your opponents choices based on your move. The recursive element of minimax occurs when you continously call the minimax algorithm until you reach a base case of 0 pieces. Nim is a good test case for this algorithm because it is a two player, zero sum, alternating moves game.
+
+## Usage
+Someone should run my program by first hard coding the number of pieces that they are playing with in simple nim. For example, in my game I decide to use six pieces in only one pile. Next, they should call runGame in a file that is in the same directory as the NimRunner.java file. Upon calling runGame with the set number of hardcoded pieces, this will call all of the other helper methods and important methods like minimax and bestmove. The return value will be a boolean, either true or fale, of whether or not the player "player X" won the game of simple nim.
+
+## Future Directions and Improvements
+An improvement that can be made to the code is to adapt simple num to work for different piles, and using multiple different pieces in each pile. While simple nim only has a set number of pieces in one pile, we can add another level of complexity by creating multiple piles with different number of pieces. We can do so by adapating the data structures that encapsulate the number of pieces that we are playing with. Another adaptation is to add user input 
+ 
