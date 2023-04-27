9505e9ca789164d8779e1147365978f4ea6315a3
Patricia von OIste <patricia@vonoiste.com>
2023-04-27 14:05:25 -0400

    added more test cases

--- Tests.java
+++ Tests.java
@@ -3,5 +3,13 @@ public class Tests{
         //test cases for NimRunner (base cases)
         System.out.println(1 == NimRunner.minimax(0, true));
         System.out.println(-1 == NimRunner.minimax(0, false));
+        System.out.println(NimRunner.runGame(6));
+        System.out.println(NimRunner.minimax(2, false) == -1); //true
+        System.out.println(NimRunner.minimax(3, false) == 1); //true
+        System.out.println(NimRunner.bestMove(1, false));
+        System.out.println(NimRunner.bestMove(4, true)); //the best case scenario would be 3 pieces to take
+        System.out.println(NimRunner.runGame(5)); //true
+        System.out.println(NimRunner.runGame(4)); //true
+
     }
 }
